c5e471cbd7d7116e54945c0bb58cd0e338297ce5
Luka-Rapaic <lukarapaic2014@gmail.com>
2024-12-15 02:52:34 +0100

    Trying to disable zoom on mobile

--- Scripts/tetris-igra.js
+++ Scripts/tetris-igra.js
@@ -15,12 +15,14 @@ new KeyDown(ctxMD).paint();
 let ctxMR = document.getElementById("ctxMR").getContext("2d");
 new KeyRight(ctxMR).paint();
 
-let tetrisBoard = document.getElementById("board");
-tetrisBoard.addEventListener("touchstart", (event) => {
-    event.preventDefault();
-});
+// let tetrisBoard = document.getElementById("board");
+// tetrisBoard.addEventListener("touchstart", (event) => {
+//     event.preventDefault();
+// });
 
-let tetrisHud = document.getElementById("tetris-hud");
-tetrisHud.addEventListener("touchstart", (event) => {
-    event.preventDefault();
+let gameOverScreen = document.getElementById("gameOverScreen");
+gameOverScreen.addEventListener("touchstart", (event) => {
+    if (event.touches.length>1) {
+        event.preventDefault();
+    }
 });
